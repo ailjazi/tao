f5d7428b438aeea646d625e444370d09ce52a624
Aldo Iljazi <mail@ailjazi.me>
2014-09-29 23:06:44 +0300

    Final version??
    Hopefuly!!

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir ~/.tao
-wget -O /etc/xdg/autostart/updater.desktop https://raw.githubusercontent.com/ailjazi/tao/master/updater.desktop
-wget -O /etc/xdg/autostart/tao.desktop https://raw.githubusercontent.com/ailjazi/tao/master/tao.desktop
+wget -O ~/.config/autostart/updater.desktop https://raw.githubusercontent.com/ailjazi/tao/master/updater.desktop
+wget -O ~/.config/autostart/tao.desktop https://raw.githubusercontent.com/ailjazi/tao/master/tao.desktop
 wget -O ~/.tao/tao.sh https://raw.githubusercontent.com/ailjazi/tao/master/tao.sh
 wget -O ~/.tao/taos https://raw.githubusercontent.com/ailjazi/tao/master/taos
 wget -O ~/.tao/yingyang.png https://raw.githubusercontent.com/ailjazi/tao/master/yingyang.png
