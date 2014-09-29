36e02b16ebaf64574442c0c14235f833e96aa968
Aldo Iljazi <mail@ailjazi.me>
2014-09-30 00:12:37 +0300

    OH MY GOD!

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 mkdir ~/.tao
 wget -O ~/.tao/tao.sh https://raw.githubusercontent.com/ailjazi/tao/master/tao.sh
 wget -O ~/.tao/taos https://raw.githubusercontent.com/ailjazi/tao/master/taos
+wget -O ~/.tao/update.sh https://raw.githubusercontent.com/ailjazi/tao/master/update.sh
 wget -O ~/.tao/yingyang.png https://raw.githubusercontent.com/ailjazi/tao/master/yingyang.png
 echo "" >> ~/.profile
 echo "bash ~/.tao/tao.sh &" >> ~/.profile
