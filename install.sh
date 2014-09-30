557a5075bf893b611ef122a2049409fe2ab5483f
ailjazi <mail@ailjazi.me>
2014-09-30 15:02:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ wget -O ~/.tao/tao.sh https://raw.githubusercontent.com/ailjazi/tao/master/tao.s
 wget -O ~/.tao/taos https://raw.githubusercontent.com/ailjazi/tao/master/taos
 wget -O ~/.tao/update.sh https://raw.githubusercontent.com/ailjazi/tao/master/update.sh
 wget -O ~/.tao/yingyang.png https://raw.githubusercontent.com/ailjazi/tao/master/yingyang.png
-echo "" >> ~/.profile
-echo "bash ~/.tao/tao.sh &" >> ~/.profile
-echo "bash ~/.tao/update.sh &" >> ~/.profile
+
+exists=$(! grep -q "bash ~/.tao/tao.sh &" .profile && echo $?)
+if [ $exists -eq 0]; then
+  echo "" >> ~/.profile
+  echo "bash ~/.tao/tao.sh &" >> ~/.profile
+  echo "bash ~/.tao/update.sh &" >> ~/.profile
+fi
